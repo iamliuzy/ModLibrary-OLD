dbbc62af44fc0c0a0e6d9fb84c5d0a781962df3e
iamliuzy <liuzhiyu.sh@outlook.com>
2023-02-10 23:45:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import constants
+import lang
+import jsonparse
 
 
 class App:
+    def init_homepage(self):
+        self.homepage_panel = tk.Frame(self.root)
+        self.homepage_panel.place(x=50, y=0,
+                                  width=self.root.winfo_width()-50,
+                                  relheight=1.0)
+        self.homepage_searchbox_text = tk.StringVar()
+        self.homepage_searchbox_entry = ttk.Entry(master=self.homepage_panel,
+                                                  textvariable=self.homepage_searchbox_text)
+        self.homepage_searchbox_entry.place(relx=0.01, y=0, relwidth=0.98, height=30)
+
     def init_sidebar(self):
         self.sidebar_panel = tk.Frame(self.root)
-        self.sidebar_download_button = tk.Button(self.sidebar_panel, command=None)
+        self.sidebar_panel.place(x=0, y=0, width=50,
+                                 relheight=1.0)
+        self.sidebar_homepage_button = ttk.Button(self.sidebar_panel,
+                                                  text=lang._("sidebar.home"),
+                                                  command=self.init_homepage)
+        self.sidebar_homepage_button.place(x=0, y=0,
+                                           relwidth=1.0,
+                                           height=50)
 
     def __init__(self):
         self.root = tk.Tk(constants.NAME)
         self.root.title(constants.NAME + " " + constants.VERSION)
-        self.root.geometry("400x400+200+200")
-
+        self.root.geometry("720x480+200+200")
+        self.settings = jsonparse.QuickAccess.json_to_dict(".\\settings.json")
+        if self.settings.get("default_page") == 0:
+            self.init_homepage()
+        elif self.settings.get("default_page") == 1:
+            pass
+        self.init_sidebar()
         self.root.mainloop()
 
 
