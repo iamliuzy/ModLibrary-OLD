2be680d3ac7b26117c1656149b7e968a3799da88
iamliuzy <liuzhiyu.sh@outlook.com>
2023-02-11 22:47:00 +0800

    添加主页内容

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import tkinter.filedialog
 import constants
 import lang
 import jsonparse
 
 
 class App:
+    def add_mod(self):
+        tkinter.filedialog.askopenfilenames(title=lang._("homepage.add_mod.title"),
+                                            filetypes=[("Mod", "*.jar")])
+
     def init_homepage(self):
         self.homepage_panel = tk.Frame(self.root)
         self.homepage_panel.place(x=50, y=0,
                                   width=self.root.winfo_width()-50,
                                   relheight=1.0)
         self.homepage_searchbox_text = tk.StringVar()
-        self.homepage_searchbox_entry = ttk.Entry(master=self.homepage_panel,
+        self.homepage_searchbox_entry = ttk.Entry(self.homepage_panel,
                                                   textvariable=self.homepage_searchbox_text)
-        self.homepage_searchbox_entry.place(relx=0.01, y=0, relwidth=0.98, height=30)
+        self.homepage_searchbox_entry.place(x=0, y=0, width=self.root.winfo_width()-75, height=30)
+        self.homepage_add_button = ttk.Button(self.homepage_panel,
+                                              text="aaa",
+                                              command=None)
+        self.homepage_add_button.place(x=self.root.winfo_width()-50-self.homepage_searchbox_entry.winfo_width(),
+                                       y=0, width=25, height=30)
 
     def init_sidebar(self):
         self.sidebar_panel = tk.Frame(self.root)
