d51335ce6a144d00dfce840baad750e9ed1f29f5
iamliuzy <liuzhiyu.sh@outlook.com>
2023-03-10 21:43:15 +0800

    修改日志模块
    - 删除不必要的参数
    - 当使用DEBUG模式时（PyCharm debug运行或MODLIB_DEBUG为非零值），输出日志。

--- log.py
+++ log.py
@@ -1,6 +1,8 @@
 import datetime
 from os import linesep
 
+import constants
+
 logfile = open("latest.log", mode="w", encoding="utf-8")
 debug_logfile = open("latest_debug.log", mode="w", encoding="utf-8")
 
@@ -18,27 +20,27 @@ def log(s: str, level=LogLevel.info):
     result = ""
     result = result + "[" + time + "]" + "(" + level + ")"
     result = result + " " + s
-    if not level == LogLevel.debug:
+    if (not level == LogLevel.debug) or constants.DEBUG:
         print(result)
     logfile.write(result + linesep)
     debug_logfile.write(result + linesep)
 
 
-def debug(s: str, source=""):
+def debug(s: str):
     log(s, LogLevel.debug)
 
 
-def info(s: str, source=""):
+def info(s: str):
     log(s, LogLevel.info)
 
 
-def warn(s: str, source=""):
+def warn(s: str):
     log(s, LogLevel.warn)
 
 
-def error(s: str, source=""):
+def error(s: str):
     log(s, LogLevel.error)
 
 
-def critical(s: str, source=""):
+def critical(s: str):
     log(s, LogLevel.critical)
