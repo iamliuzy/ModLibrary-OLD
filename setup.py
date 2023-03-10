6a3de44a1625c94c5f8ef6d4bdca688ac3f546ec
iamliuzy <liuzhiyu.sh@outlook.com>
2023-03-10 21:40:40 +0800

    更新cx_Freeze配置文件
    还是不能用

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from cx_Freeze import setup, Executable
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
 
-setup(executables=[Executable("*.py", base=base)])
+setup(executables=[Executable("main.py", base=base)])
