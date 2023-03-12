4dca0c87c971d61cfdb8ecde52d7d8577808aa6e
iamliuzy <liuzhiyu.sh@outlook.com>
2023-03-12 23:01:15 +0800

    重命名编译后脚本
    下周再来弄吧

--- after_build.py
+++ after_build.py
@@ -1,5 +1,7 @@
-import toml, shutil
+import toml, shutil, re
 from os.path import abspath
+
+
 
 for file in list(dict(dict(toml.loads(open("pyproject.toml", "r", encoding="utf-8").read())).get("tool.distutils.build_exe")).get("include_files")):
     shutil.copy(abspath(file), abspath(".\\dist\\"))
