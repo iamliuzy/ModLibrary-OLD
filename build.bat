1d6fb1c6de46764856e9b70575e426db3710ce09
iamliuzy <liuzhiyu.sh@outlook.com>
2023-04-30 01:45:21 +0800

    Update build scripts
    It doesn't work. I'll continue on this tomorrow.

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ cd __pycache__
 set "str=.cpython-310.opt-2"
 setlocal EnableDelayedExpansion
 for /f "delims=" %%i in ('dir /b *.cpython-311.opt-2.pyc') do (
-set "var=%%i" & ren "%%i" "!var:%str%=!")
+set "var=%%i"
+ren "%%i" "!var:%str%=!")
 cd ..
 xcopy pack_resources __pycache__ /E /Y
--- build.sh
+++
@@ -1,4 +0,0 @@
-python -m compileall . -l -f -i build.txt -o 2
-cd __pycache__ || exit
-rename cpython-*.pyc .pyc *.pyc
-cp -r pack_resources/ __pycache__
